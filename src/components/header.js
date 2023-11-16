d95e2dc369f9799ba4473aaa4fbf5a5b0fce47e3
Hugo Depoumps <depoumpshugo@gmail.com>
2023-11-16 08:53:01 +0100

    Initialization of the header components and the scss mainPage. Delete unnecessary files

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+const tempsEnMs = Date();
+console.log(tempsEnMs)
+function Header() {
+    return (
+        <header className="App-header">
+            <p>Hello</p>
+        </header>
+    );
+}
+export default Header;
---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import '../style/css/mainPage.css';
+import Header from "../components/header";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
